e9ec6c619cdd15991536d800703a63b1523b5164
Jacob Naylor <jacob.naylor@k-int.co.uk>
2023-09-21 14:38:13 +0100

    WIP: Forked to restructure for Maven project

--- src/ro/webdata/parser/xml/lido/core/ParserDAO.java
+++
@@ -1,7 +0,0 @@
-package ro.webdata.parser.xml.lido.core;
-
-import ro.webdata.parser.xml.lido.core.wrap.lidoWrap.LidoWrap;
-
-public interface ParserDAO {
-    LidoWrap parseLidoFile(String filePath);
-}
